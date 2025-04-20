1c227e1b11a52c637dd428670d3868e84afdd856
hoanhat0305 <dangnhathoa22@gmail.com>
2025-04-20 18:25:38 +0700

    commit

--- app/database/elasticsearch.py
+++ app/database/elasticsearch.py
@@ -3,10 +3,9 @@ from elasticsearch import Elasticsearch
 
 class ElasticsearchClient:
     def __init__(self, host='localhost', port=9200, username='elastic', password=None):
-        ctx = ssl.SSLContext(ssl.PROTOCOL_TLS_CLIENT)
+        ctx = ssl.create_default_context()
         ctx.load_verify_locations('./setup/ca.crt')
-        ctx.check_hostname = False
-        ctx.verify_mode = ssl.CERT_REQUIRED
+        ctx.verify_flags &= ~ssl.VERIFY_X509_STRICT
         print("host: ", host)
         print("port: ", port)
         print("username: ", username)
@@ -15,7 +14,7 @@ class ElasticsearchClient:
             self.__es = Elasticsearch([{'host': host, 'port': port, 'scheme': 'https'}],
                                     http_auth=(username, password),
                                     verify_certs=True,
-                                    ssl_context=ctx)
+                                    ca_certs='./setup/ca.crt')
         except Exception as e:
             print(f"Error connecting to Elasticsearch: {e}")
             raise Exception(f"Could not connect to Elasticsearch: {e}")
