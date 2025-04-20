ac42c385fb7d496f41aed33128a7feebd7eadd65
hoanhat0305 <dangnhathoa22@gmail.com>
2025-04-20 17:00:00 +0700

    fix syntax

---
+++ app/database/elasticsearch.py
@@ -0,0 +1,22 @@
+import ssl
+from elasticsearch import Elasticsearch
+
+class ElasticsearchClient:
+    def __init__(self, host='localhost', port=9200, username='elastic', password=None):
+        ctx = ssl.create_default_context()
+        ctx.load_verify_locations('./setup/ca.crt')
+        ctx.verify_flags &= ~ssl.VERIFY_X509_STRICT
+
+        self.__es = Elasticsearch([{'host': host, 'port': port, 'scheme': 'https'}],
+                                   http_auth=(username, password),
+                                   verify_certs=True,
+                                   ssl_context=ctx)
+        
+        if not self.ping():
+            raise Exception("Could not connect to Elasticsearch")
+        
+    def ping(self):
+        return self.__es.ping()
+
+    def get_client(self):
+        return self.__es
