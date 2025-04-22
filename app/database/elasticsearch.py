9e479e9f795629f1bcec0f95fd6501d9cb1e8934
hoanhat0305 <dangnhathoa22@gmail.com>
2025-04-22 18:53:19 +0700

    set cart back

--- app/database/elasticsearch.py
+++ app/database/elasticsearch.py
@@ -3,13 +3,11 @@ from elasticsearch import Elasticsearch
 
 class ElasticsearchClient:
     def __init__(self, host='localhost', port=9200, username='elastic', password=None):
-        print("host: ", host)
-        print("port: ", port)
-        print("username: ", username)
-        print("password: ", password)
         try:
-            self.__es = Elasticsearch([{'host': host, 'port': port, 'scheme': 'http'}],
-                                    http_auth=(username, password))
+            self.__es = Elasticsearch([{'host': host, 'port': port, 'scheme': 'https'}],
+                                    http_auth=(username, password),
+                                    ssl_context=ssl.create_default_context(cafile="./setup/ca.crt"),
+                                    verify_certs=True)
         except Exception as e:
             print(f"Error connecting to Elasticsearch: {e}")
             raise Exception(f"Could not connect to Elasticsearch: {e}")
