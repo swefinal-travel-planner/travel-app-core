6b4344cd529636ec8776a81859183d324847cc71
hoanhat0305 <dangnhathoa22@gmail.com>
2025-04-28 01:15:12 +0700

    update syntax

--- app/database/elasticsearch.py
+++ app/database/elasticsearch.py
@@ -2,12 +2,11 @@ import ssl
 from elasticsearch import Elasticsearch
 
 class ElasticsearchClient:
-    def __init__(self, host='103.72.97.222', port=9200, username='elastic', password=None):
+    def __init__(self, host, port, username, password):
         print(f"Connecting to Elasticsearch at {host}:{port} with user {username} and password {password}")
         try:
             self.__es = Elasticsearch([{'host': host, 'port': port, 'scheme': 'http'}],
                                     http_auth=(username, password),
-                                    # ssl_context=ssl.create_default_context(cafile="./setup/ca.crt"),
                                     verify_certs=False)
         except Exception as e:
             print(f"Error connecting to Elasticsearch: {e}")
