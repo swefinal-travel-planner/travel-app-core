8c15bc90133cb5bda78e47f1a682c6de732b916a
hoanhat0305 <dangnhathoa22@gmail.com>
2025-04-28 09:18:46 +0700

    fixed issue

--- app/database/elasticsearch.py
+++ app/database/elasticsearch.py
@@ -6,15 +6,11 @@ class ElasticsearchClient:
         print(f"Connecting to Elasticsearch at {host}:{port} with user {username} and password {password}")
         try:
             self.__es = Elasticsearch([{'host': host, 'port': port, 'scheme': 'http'}],
-                                    http_auth=(username, password),
+                                    basic_auth=(username, password),
                                     verify_certs=False)
             
             if self.__es.ping():
                 print("Connected to Elasticsearch successfully.")
-            else:
-                print("Could not connect to Elasticsearch.")
-                print(self.__es.info())
-                raise Exception("Could not connect to Elasticsearch")
         except Exception as e:
             print(f"Error connecting to Elasticsearch: {e}")
             raise Exception(f"Could not connect to Elasticsearch: {e}")
